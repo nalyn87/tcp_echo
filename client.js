2c6a2e0a2e8b7645d56749f1e30612c9729be067
nalyn87 <nalyn0807@gmail.com>
2024-06-24 12:36:40 +0900

    ✨ update - 주특기 플러스 강의 1-6

--- client.js
+++ client.js
@@ -10,25 +10,25 @@ const client = new net.Socket();
 client.connect(PORT, HOST, () => {
   console.log(`Connected to the server ${HOST}:${PORT}`);
 
-  const message = "Hello";
+  const message = 'Hello';
   const buffer = Buffer.from(message);
 
-  const header = writeHeader(buffer.length, 10);
-  const packet = Buffer.concat([header, buffer])
+  const header = writeHeader(buffer.length, 11);
+  const packet = Buffer.concat([header, buffer]);
   client.write(packet);
 });
 
 client.on('data', (data) => {
-    const buffer = Buffer.from(data);
+  const buffer = Buffer.from(data);
 
-    const {length, handlerId} = readHeader(buffer);
-    console.log(`handlerId: ${handlerId}`);
-    console.log(`length: ${length}`);
+  const { length, handlerId } = readHeader(buffer);
+  console.log(`handlerId: ${handlerId}`);
+  console.log(`length: ${length}`);
 
-    const headerSize = TOTAL_LENGTH_SIZE + HANDLER_ID;
-    const message = buffer.slice(headerSize);
+  const headerSize = TOTAL_LENGTH_SIZE + HANDLER_ID;
+  const message = buffer.slice(headerSize);
 
-    console.log(`server 에게 받은 메세지: ${message}`)
+  console.log(`server 에게 받은 메세지: ${message}`);
 });
 
 client.on('close', () => {
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import net from 'net';
 import { readHeader, writeHeader } from './util.js';
-import { HANDLER_ID, TOTAL_LENGTH_SIZE } from './constants.js';
+import { HANDLER_ID, MAX_MESSAGE_LENGTH, TOTAL_LENGTH_SIZE } from './constants.js';
+import handlers from './handlers/index.js';
 
 const PORT = 3000;
 
@@ -10,16 +11,32 @@ const server = net.createServer((socket) => {
   socket.on('data', (data) => {
     const buffer = Buffer.from(data);
 
-    const {length, handlerId} = readHeader(buffer);
+    const { length, handlerId } = readHeader(buffer);
     console.log(`handlerId: ${handlerId}`);
     console.log(`length: ${length}`);
+
+    if (length > MAX_MESSAGE_LENGTH) {
+      console.log(`Error: Message length ${length}`);
+      socket.write(`Error: Message is too long`);
+      socket.end();
+      return;
+    }
+
+    const handler = handlers[handlerId];
+
+    if (!handler) {
+      console.log(`Error: No handler found for ID ${handlerId}`);
+      socket.write(`Error: Invalid handler ID ${handlerId}`);
+      socket.end();
+      return;
+    }
 
     const headerSize = TOTAL_LENGTH_SIZE + HANDLER_ID;
     const message = buffer.slice(headerSize);
 
-    console.log(`client 에게 받은 메세지: ${message}`)
+    console.log(`client 에게 받은 메세지: ${message}`);
 
-    const responseMessage = "Hi, There";
+    const responseMessage = handler(message);
     const responseBuffer = Buffer.from(responseMessage);
 
     const header = writeHeader(responseBuffer.length, handlerId);
